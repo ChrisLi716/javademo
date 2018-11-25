0bcf2290e0c578fef32480a75e0a58a1b0eb3b54
chris <lilunlogic@163.com>
2018-11-25 13:34:40 +0800

    add file processor\

---
+++ src/main/com/chris/demo/ExtractWords.java
@@ -0,0 +1,36 @@
+package com.chris.demo;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.*;
+
+public class ExtractWords {
+
+    public static void main(String[] args) throws Exception {
+        String newLineSeperator = System.getProperty("line.separator");
+
+        String inputFileName = "C:\\Users\\ad\\Desktop\\words.txt";
+        String outputFileName = "C:\\Users\\ad\\Desktop\\newWords.txt";
+        FileReader fileReader = new FileReader(new File(inputFileName));
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String oneLine = null;
+        int skipLines = 4;
+        StringBuilder sb = new StringBuilder();
+
+        do {
+            for (int i = 0; i <= skipLines; i++) {
+                oneLine = bufferedReader.readLine();
+            }
+            if (StringUtils.isNotEmpty(oneLine) && !sb.toString().contains(oneLine + "|")) {
+                oneLine = oneLine.replace(",", "");
+                sb.append(oneLine).append("|");
+            }
+        }
+        while (null != oneLine);
+
+        OutputStream outputStream = new FileOutputStream(new File(outputFileName));
+        String wantedString = sb.toString().replaceAll("\\|", newLineSeperator);
+        outputStream.write(wantedString.getBytes("UTF-8"));
+        outputStream.close();
+    }
+}
