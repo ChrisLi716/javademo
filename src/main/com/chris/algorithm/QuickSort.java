5306c9975e5d343dbc93f04f434bcb5fd7fd2905
Chris <lilunlogic@163.com>
2018-12-27 12:44:00 +0800

    Test quicksort 001

--- src/main/com/chris/algorithm/QuickSort.java
+++ src/main/com/chris/algorithm/QuickSort.java
@@ -1,5 +1,7 @@
 package com.chris.algorithm;
 
+import java.util.Arrays;
+
 /**
  * @Auther Chris Lee
  * @Date 12/26/2018 15:19
@@ -9,8 +11,9 @@ public class QuickSort {
 	
 	public static void main(String[] args) {
 		QuickSort quickSort = new QuickSort();
-		int[] array = {6, 1, 3, 4, 5};
-		quickSort.quickSort(array, 0, 4);
+		int[] array = {11, 19, 16, 8, 3, 8, 18, 16, 0, 1, 18, 19, 13, 15, 6, 12, 11, 8, 7, 3};
+		CommonUtils.printArray2Str(array);
+		quickSort.quickSort(array, 0, array.length - 1);
 	}
 	
 	private void quickSort(int[] array, int lowerIndex, int higherIndex) {
@@ -19,7 +22,7 @@ public class QuickSort {
 		
 		// calculate pivot number, I am taking pivot as middle index number
 		int pivot = array[lowerIndex + (higherIndex - lowerIndex) / 2];
-		System.out.println("pivot:" + pivot + ",i:" + i + ",j:" + j);
+		System.out.println("i=" + i + ",j=" + j + ",pivot=" + pivot);
 		// Divide into two arrays
 		while (i <= j) {
 			/*
@@ -34,7 +37,7 @@ public class QuickSort {
 			while (array[j] > pivot) {
 				j--;
 			}
-			
+
 			if (i < j) {
 				int temp = array[i];
 				array[i] = array[j];
@@ -44,9 +47,8 @@ public class QuickSort {
 				j--;
 			}
 		}
-		
+        System.out.println("the stop index, i=" + i + ",j=" + j);
 		CommonUtils.printArray2Str(array);
-		
 		// call quickSort() method recursively
 		if (lowerIndex < j) {
 			quickSort(array, lowerIndex, j);
