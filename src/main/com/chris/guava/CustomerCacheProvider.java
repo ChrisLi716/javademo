c7eabf7674031f70ef402a3ae0526fc6b878d7a1
Chris <lilunlogic@163.com>
2019-01-03 12:02:53 +0800

    add guava package

---
+++ src/main/com/chris/guava/CustomerCacheProvider.java
@@ -0,0 +1,36 @@
+package com.chris.guava;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Auther Chris Lee
+ * @Date 1/3/2019 10:50
+ * @Description
+ */
+public enum CustomerCacheProvider
+{
+	INSTANCE;
+	private LoadingCache<Integer, Customer> customerCache;
+	
+	private CustomerRepo customerRepo;
+	
+	private CustomerCacheProvider()
+	{
+		customerRepo = new CustomerRepo();
+		customerCache =
+			CacheBuilder.newBuilder().maximumSize(100).expireAfterWrite(5, TimeUnit.MINUTES).build(new CacheLoader<Integer, Customer>() {
+				@Override
+				public Customer load(Integer id) {
+					return customerRepo.findById(id);
+				}
+			});
+	}
+	
+	public LoadingCache<Integer, Customer> getCustomerCache() {
+		return customerCache;
+	}
+}
