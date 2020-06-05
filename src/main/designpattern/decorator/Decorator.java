cc7d51be592714381ff6c8e1f94d0b962c75185d
chris <lilunlogic@163.com>
2020-06-05 18:16:05 +0800

    'decorator'

---
+++ src/main/designpattern/decorator/Decorator.java
@@ -0,0 +1,7 @@
+package designpattern.decorator;
+
+public abstract class Decorator implements Component {
+
+	protected Component component;
+
+}
---
+++ src/main/designpattern/decorator/Phone.java
@@ -0,0 +1,14 @@
+package designpattern.decorator;
+
+public class Phone implements Component {
+
+	@Override
+	public int cost() {
+		return 2000;
+	}
+
+	@Override
+	public void desc() {
+		System.out.println("bare phone");
+	}
+}
