fac37150fd45d0e10ad48b8f551b384be038d809
Chris <lilunlogic@163.com>
2020-11-08 22:19:46 +0800

    StreamAPI Creator

---
+++ src/main/com/chris/lambda/StreamCreator.java
@@ -0,0 +1,64 @@
+package com.chris.lambda;
+
+import com.chris.lambda.method.reference.Employee;
+import com.chris.lambda.method.reference.EmployeeData;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class StreamCreator {
+
+    /**
+     * 通过集合创建Stream
+     */
+    public void createStreamByCollection() {
+        List<Employee> employees = EmployeeData.getEmployees();
+        //返回一个顺序流
+        Stream<Employee> stream = employees.stream();
+
+        //返回一个并行流
+        Stream<Employee> parallelStream = employees.parallelStream();
+    }
+
+
+    /**
+     * 通过数组创建Stream
+     */
+    public void createStreamByArrays() {
+        int[] intArr = new int[]{1, 2, 3, 4, 5, 6};
+        IntStream stream = Arrays.stream(intArr);
+
+        Employee chris = new Employee(1, "Chris", 23, 23000.43);
+        Employee john = new Employee(2, "John", 14, 3000.43);
+        Employee[] empArr = new Employee[]{chris, john};
+
+        Stream<Employee> empStream = Arrays.stream(empArr);
+
+    }
+
+    /**
+     * 通过Stream的of()方法创建Steam
+     */
+    public void createStreamByStream() {
+        Stream<Integer> stream = Stream.of(1, 2, 3, 4, 5, 6);
+    }
+
+    /**
+     * 创建无限流
+     */
+    public void createStreamByStream2() {
+        /*
+         迭代
+         创建并遍历前10个偶数
+         */
+        Stream.iterate(0, seed -> seed + 2).limit(10).forEach(System.out::println);
+
+        /*
+         生成
+         创建并遍历前10个偶数
+         */
+        Stream.generate(Math::random).limit(10).forEach(System.out::println);
+    }
+}
